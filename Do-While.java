5d18a5fe77a4a3d9428ce35ea5550a166d3202b6
KC-DEEPAK <deepakkcdeepu77@gmail.com>
2026-02-08 15:33:17 +0530

    Do_While_loop

---
+++ Do-While.java
@@ -0,0 +1,15 @@
+//we have seen while loop now we use do-while loop in which helps ..
+// it atleast execute the block at onces in loop..
+class Do_while
+{
+  public static void main(String args[])
+  {
+    int n=6;
+    do
+    {
+      System.out.println("hello "+n);
+      n++;
+    }while(n<=5);
+  }// if i enter the 6 to the it exectue onces which gives hello 6...
+  
+}
